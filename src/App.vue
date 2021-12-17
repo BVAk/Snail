d414a8e62783da09cd85de5b7b63a383ae8beab1
Viktoriia Romanova <viktoriia@MacBoochichek.local>
2021-12-17 19:24:14 +0200

    create pixi vue3 project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="connections">
+    <canvas id="pixi"></canvas>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as PIXI from 'pixi.js'
+window.PIXI = PIXI;
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  methods: {
+    drawPixi() {
+      var canvas = document.getElementById('pixi')
+
+      const app = new PIXI.Application({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        antialias: true,
+        backgroundAlpha: true,
+        view: canvas,
+      })
+
+      let graphics = new PIXI.Graphics() 
+      graphics.lineStyle(8, 0x000000)
+
+      //start
+      graphics.moveTo(300, 250)
+      //end
+      graphics.lineTo(500, 250)
+
+      app.stage.addChild(graphics)
+    },
+  },
+
+  mounted() {
+    this.drawPixi()
+  },
 }
 </script>
 
